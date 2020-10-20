270594150aa244a1142f0456c626e604deb8d95b
Tata Taerattanachai <2506139t@student.gla.ac.uk>
2020-10-21 00:32:20 +0100

    Sample scene createddd

---
+++ Assets/Scripts/LookX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookX : MonoBehaviour
+{
+    [SerializeField]
+    private float _sensitivity = 2f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * _sensitivity;
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y + mouseX ,transform.localEulerAngles.z);
+
+        
+    }
+}
