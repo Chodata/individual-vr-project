f7ef2dce420bfdad538dca8ecc69ddef1838045c
Tata Taerattanachai <2506139t@student.gla.ac.uk>
2021-01-10 23:44:46 +0700

    Add Raycasting example for the project

---
+++ Assets/Scripts/OcclusionManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OcclusionManager : MonoBehaviour
+{
+    public MyInterface myInterface;
+    public MyObject myObject;
+    public Camera camera;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 startRay = myObject.transform.position;
+        Vector3 endRay = myObject.transform.TransformDirection(camera.transform.position - myObject.transform.position);
+        if (Physics.Raycast(startRay, endRay, out hit, 100))
+        {
+            Debug.DrawRay(startRay, endRay * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+
+        }
+        else
+        {
+            Debug.DrawRay(startRay, endRay  * 1000, Color.white);
+            Debug.Log("Did not Hit");
+        }
+    }
+}
